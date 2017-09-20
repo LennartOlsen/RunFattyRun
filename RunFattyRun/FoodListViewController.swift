cf51d907aa68e23b51464cbf047323765ce654b7
Lennart Olsen <lo@cardlay.com>
2017-09-20 09:06:06 +0200

    Adding views and stuffs

---
+++ RunFattyRun/FoodListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FoodListViewController.swift
+//  RunFattyRun
+//
+//  Created by Lennart Olsen on 20/09/2017.
+//  Copyright © 2017 Burger Inc. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class FoodListViewController: UIViewController {
+    
+    @IBOutlet var picker : UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ RunFattyRun/food.swift
@@ -0,0 +1,16 @@
+//
+//  food.swift
+//  RunFattyRun
+//
+//  Created by Lennart Olsen on 20/09/2017.
+//  Copyright © 2017 Burger Inc. All rights reserved.
+//
+
+import Foundation
+
+class food {
+    let name : String = ""
+    let description : String? = nil
+    let kalories : Int = 0
+    let image : String = "" /** Should be a reference to the burger image **/
+}
